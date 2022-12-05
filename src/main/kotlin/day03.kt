5b77e215c90c2ecc777a1f32790f31f3fbbc7d2e
Jérémy PEPIN <pepin.jeremy81@gmail.com>
2022-12-05 23:09:02 +0100

    feat(day03): part two

--- src/main/kotlin/day03.kt
+++ src/main/kotlin/day03.kt
@@ -11,10 +11,10 @@ fun main() {
     check(answerPartOneSmallInput == 157)
     println("Answer to part one (large input): ${day03.partOne(largeInput)}.")
 
-//    val answerPartTwoSmallInput = day03.partTwo(smallInput)
-//    println("Answer to part two (small input): ${answerPartTwoSmallInput}.")
-//    check(answerPartTwoSmallInput == 12)
-//    println("Answer to part two (small input): ${day03.partTwo(largeInput)}.")
+    val answerPartTwoSmallInput = day03.partTwo(smallInput)
+    println("Answer to part two (small input): ${answerPartTwoSmallInput}.")
+    check(answerPartTwoSmallInput == 70)
+    println("Answer to part two (small input): ${day03.partTwo(largeInput)}.")
 }
 
 class Day03 {
@@ -22,19 +22,35 @@ class Day03 {
         val misplacedItems = mutableListOf<Char>()
 
         input.forEach {
-            misplacedItems.add(this.getMisplacedItem(it))
+            val compartmentContent = it.chunked(it.length / 2)
+            misplacedItems.add(
+                this.getMisplacedItem(
+                    compartmentContent[0].toCharArray(),
+                    compartmentContent[1].toCharArray()
+                ).first()
+            )
         }
 
-        return misplacedItems.sumOf { if (it.isUpperCase()) it.code - 38 else it.code - 96 }
+        return misplacedItems.sumOf { this.getItemValue(it) }
+    }
+
+    private fun getMisplacedItem(firstItem: CharArray, secondItem: CharArray): CharArray {
+        return firstItem.toList().intersect(secondItem.toList().toSet()).toCharArray()
     }
 
     fun partTwo(input: List<String>): Int {
-        return -1
+        val badges = mutableListOf<Char>()
+
+        input.chunked(3).forEach { items ->
+            val badge = items.map { it.toCharArray() }.reduce { acc, item -> this.getMisplacedItem(item, acc) }.first()
+            badges.add(badge)
+        }
+
+        return badges.sumOf { this.getItemValue(it) }
     }
 
-    private fun getMisplacedItem(items: String): Char {
-        val compartmentContent = items.chunked(items.length / 2)
-        return compartmentContent[0].toList().intersect(compartmentContent[1].toList().toSet()).first()
+    private fun getItemValue(item: Char): Int {
+        return if (item.isUpperCase()) item.code - 38 else item.code - 96
     }
 
     fun getSmallInput(): List<String> {
