97dd462d535e50488825f3553f66c8252b787680
Jérémy PEPIN <pepin.jeremy81@gmail.com>
2022-12-06 23:02:29 +0100

    feat(day05): part two

--- src/main/kotlin/day05.kt
+++ src/main/kotlin/day05.kt
@@ -15,10 +15,10 @@ fun main() {
     check(answerPartOneSmallInput == "CMZ")
     println("Answer to part one (large input): ${day05.partOne(largeInput)}.")
 
-//    val answerPartTwoSmallInput = day05.partTwo(smallInput)
-//    println("Answer to part two (small input): ${answerPartTwoSmallInput}.")
-//    check(answerPartTwoSmallInput == "")
-//    println("Answer to part two (small input): ${day05.partTwo(largeInput)}.")
+    val answerPartTwoSmallInput = day05.partTwo(smallInput)
+    println("Answer to part two (small input): ${answerPartTwoSmallInput}.")
+    check(answerPartTwoSmallInput == "MCD")
+    println("Answer to part two (small input): ${day05.partTwo(largeInput)}.")
 }
 
 class Day05 {
@@ -35,7 +35,15 @@ class Day05 {
     }
 
     fun partTwo(input: List<String>): String {
-        return ""
+        val stacks = this.getStacks(input)
+        val moves = this.getMoves(input)
+
+        moves.forEach {
+            this.moveAndPreserveOrder(it.number, stacks[it.sourceStackIndex], stacks[it.destinationStackIndex])
+        }
+
+
+        return stacks.map { it.getLast() }.joinToString("")
     }
 
     private fun getStacks(input: List<String>): Array<Stack> {
@@ -67,6 +75,17 @@ class Day05 {
     private fun move(number: Int, sourceStack: Stack, destinationStack: Stack) {
         repeat(number) {
             destinationStack.addCrate(sourceStack.removeCrate())
+        }
+    }
+
+    private fun moveAndPreserveOrder(number: Int, sourceStack: Stack, destinationStack: Stack) {
+        var cratesRemoved = emptyArray<Char>()
+        repeat(number) {
+            cratesRemoved += sourceStack.removeCrate()
+        }
+
+        cratesRemoved.reversed().forEach {
+            destinationStack.addCrate(it)
         }
     }
 
